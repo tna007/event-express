9f2a1a661384a08cff21c83932f9d1351c0b7099
Ankita Bhatnagar <ankita.bhatnagar@outlook.com>
2021-05-23 20:11:38 +0200

    initializing backend

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var router = express.Router();
+const fetch = require("node-fetch");
+
+/* GET Events page. */
+router.get("/events", async function (req, res, next) {
+  let jsonResponse = await getDataFromOpenAPI("events");
+  res.json(jsonResponse);
+});
+
+/* GET Activities page. */
+router.get("/activities", async function (req, res, next) {
+  let jsonResponse = await getDataFromOpenAPI("activities");
+  res.json(jsonResponse);
+});
+
+function getDataFromOpenAPI(type) {
+  let URL = "";
+  switch (type) {
+    case "events":
+      URL = "http://open-api.myhelsinki.fi/v1/events/?language_filter=en&limit=20";
+      break;
+      case "activities":
+        URL = "http://open-api.myhelsinki.fi/v1/activities/?language_filter=en&limit=5";
+      break;
+  
+    default:
+      break;
+  }
+  return fetch(URL)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      return responseJson;
+    });
+}
+
+module.exports = router;
+
